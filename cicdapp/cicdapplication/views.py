3c777e9f45ee99e74b08b4721c120dfe184149e4
kev1226 <150808451+kev1226@users.noreply.github.com>
2024-12-16 09:11:49 -0500

    TEST AMI

--- cicdapp/cicdapplication/views.py
+++ cicdapp/cicdapplication/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     # Obtener el host desde la cabecera HTTP
     host = request.get_host()
-    return HttpResponse(f"repaso testing prueba 16-12-2024 load balancer - Host: {host} ")
+    return HttpResponse(f"repaso testing prueba 16-12-2024 AMI load balancer - Host: {host} ")
