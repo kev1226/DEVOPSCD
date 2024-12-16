3629691b8978da1def216ed13a2de742eeeeef20
kev1226 <150808451+kev1226@users.noreply.github.com>
2024-12-16 00:50:11 -0500

    get host

--- cicdapp/cicdapplication/views.py
+++ cicdapp/cicdapplication/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("repaso testing prueba load balancer")
+    # Obtener el host desde la cabecera HTTP
+    host = request.get_host()
+    return HttpResponse(f"repaso testing prueba load balancer - Host: {host}")
